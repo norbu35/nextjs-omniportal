6b12dbe78adde2de14027097d8438c1eb5f5a163
norbu <93921032+norbu35@users.noreply.github.com>
2023-08-03 21:00:38 +0800

    add custom login page css

--- components/forms/Login.tsx
+++ components/forms/Login.tsx
@@ -4,6 +4,16 @@ export default function Login() {
   return (
     <div className={styles.container}>
       <div className={styles.innerContainer}>
+        <form className={styles.emailAuth}>
+          <label className={styles.input}>
+            <input type="email" placeholder='Email address' required />
+          </label>
+          <label className={styles.input}>
+            <input type="password" placeholder='Password' required />
+          </label>
+          <button className={styles.submitButton} type="submit">Login</button>
+        </form>
+        <div className={styles.divider}>OR</div>
         <div className={styles.authProviders}>
           <div className={styles.providerButton}>
             <img
@@ -30,9 +40,6 @@ export default function Login() {
             <div className="providerName">Continue with Github</div>
           </div>
         </div>
-        <hr className={styles.divider} />
-        <div className={styles.dividerText}>OR</div>
-        <div className="emailAuth"></div>
       </div>
     </div>
   );
