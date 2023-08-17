ed6773923eda13afe82b087ec49c29d3aa9f03a7
norbu <93921032+norbu35@users.noreply.github.com>
2023-08-17 11:12:54 +0800

    extracted authOptions into a separate file

---
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,75 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { AuthOptions } from 'next-auth';
+import bcryptjs from 'bcryptjs';
+import GithubProvider from 'next-auth/providers/github';
+import FacebookProvider from 'next-auth/providers/facebook';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { Adapter } from 'next-auth/adapters';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '@/prisma/client';
+
+const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  pages: {
+    signIn: '/login',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID!,
+      clientSecret: process.env.FACEBOOK_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'password',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'jdoe@example.com',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'hunter2',
+        },
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (
+          !user ||
+          !(await bcryptjs.compare(credentials.password, user.password!))
+        ) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+        };
+      },
+    }),
+  ],
+};
+
+export default authOptions;
